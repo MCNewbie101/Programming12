d35da3a5b02ffec271641ea49fc9abb902de48d3
MCNewbie101 <94023224+MCNewbie101@users.noreply.github.com>
2022-03-31 14:57:07 -0700

    Finish Fibo

--- Progaming12/src/Fibonacci/Fibo.java
+++ Progaming12/src/Fibonacci/Fibo.java
@@ -2,9 +2,9 @@ package Fibonacci;
 
 public class Fibo {
     public int fibo(int remainTurns, int a, int b) {
-        if (remainTurns == 1) {
+        if (remainTurns == 0) {
             return b;
         }
-        return fibo(remainTurns, b, a + b);
+        return fibo(remainTurns - 1, b, a + b);
     }
 }
--- Progaming12/src/Fibonacci/Main.java
+++ Progaming12/src/Fibonacci/Main.java
@@ -1,5 +1,8 @@
 package Fibonacci;
 
 public class Main {
-    Fibo fib = new Fibo();
+    public static void main(String[] args) {
+        Fibo fibo = new Fibo();
+        System.out.println(fibo.fibo(6, 1, 1));
+    }
 }

12ffa920caf99a88075852933d1a7e84e374939d
MCNewbie101 <94023224+MCNewbie101@users.noreply.github.com>
2022-04-13 14:15:09 -0700

    Finish Merge Sort

--- Progaming12/src/Sort/Main.java
+++ Progaming12/src/Sort/Main.java
@@ -7,5 +7,7 @@ public class Main {
         MergeSort merge = new MergeSort();
         int[] a = merge.mergeAscending(new int[]{1, 2, 5, 7, 10, 35}, new int[]{1, 2, 3, 9, 10, 25});
         System.out.println(Arrays.toString(a));
+        int[] b = {1, 5, 3, 6, 2, 0, 9};
+        System.out.println(Arrays.toString(merge.sortDescending(b)));
     }
 }
--- Progaming12/src/Sort/MergeSort.java
+++ Progaming12/src/Sort/MergeSort.java
@@ -4,6 +4,28 @@ public class MergeSort {
     public MergeSort() {
 
     }
+    public int[] sortAscending(int[] arr) {
+        if (arr.length <= 1) {
+            return arr;
+        }
+        int i = arr.length / 2;
+        int i2 = 0;
+        int[] left = new int[arr.length / 2];
+        for (int i1 = 0; i1 < i; i1++) {
+            left[i1] = arr[i2];
+            i2++;
+        }
+        left = sortAscending(left);
+        i = arr.length - arr.length / 2;
+        int[] right = new int[arr.length - arr.length / 2];
+        for (int i1 = 0; i1 < i; i1++) {
+            right[i1] = arr[i2];
+            i2++;
+        }
+        right = sortAscending(right);
+        return mergeAscending(left, right);
+    }
+
     public int[] mergeAscending(int[] arr1, int[] arr2) {
         int[] result = new int[arr1.length + arr2.length];
         int i1 = 0;
@@ -30,6 +52,28 @@ public class MergeSort {
         return result;
     }
 
+    public int[] sortDescending(int[] arr) {
+        if (arr.length <= 1) {
+            return arr;
+        }
+        int i = arr.length / 2;
+        int i2 = 0;
+        int[] left = new int[arr.length / 2];
+        for (int i1 = 0; i1 < i; i1++) {
+            left[i1] = arr[i2];
+            i2++;
+        }
+        left = sortDescending(left);
+        i = arr.length - arr.length / 2;
+        int[] right = new int[arr.length - arr.length / 2];
+        for (int i1 = 0; i1 < i; i1++) {
+            right[i1] = arr[i2];
+            i2++;
+        }
+        right = sortDescending(right);
+        return mergeDescending(left, right);
+    }
+
     public int[] mergeDescending(int[] arr1, int[] arr2) {
         int[] result = new int[arr1.length + arr2.length];
         int i1 = 0;
@@ -41,12 +85,12 @@ public class MergeSort {
                 continue;
             }
             if (i2 >= arr2.length) {
-                result[i] = arr2[i1];
+                result[i] = arr1[i1];
                 i1++;
                 continue;
             }
             if (arr1[i1] > arr2[i2]) {
-                result[i1] = arr1[i1];
+                result[i] = arr1[i1];
                 i1++;
             } else  {
                 result[i] = arr2[i2];

d038f4526ffdfe7c6f34dc38081d1b71f5ce5fb0
MCNewbie101 <94023224+MCNewbie101@users.noreply.github.com>
2022-05-11 15:04:50 -0700

    update

--- FilesAndDatabases/src/main/java/libraries/Main.java
+++ FilesAndDatabases/src/main/java/libraries/Main.java
@@ -2,12 +2,15 @@ package libraries;
 
 import java.util.Scanner;
 import org.apache.derby.database.Database;
+import java.sql.*;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input directory to search: ");
         DirectoryDump directoryDump = new DirectoryDump(scanner.nextLine());
         directoryDump.dump();
+
     }
 }
